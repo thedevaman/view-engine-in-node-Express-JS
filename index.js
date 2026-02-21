8b9fb73303d48ad074f10e9a6e44387e99011d4b
thedevaman <devaman2508@gmail.com>
2026-02-21 09:01:17 +0530

    ejs installed and html file rendered with dynamic data

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+  
+    res.render('home',{name:'Test Singh',contact_no: 1478523690,email:'test@gamil.com'})
+
+})
+
+
+app.listen(3200)
