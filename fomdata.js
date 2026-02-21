d371de241facd3db44fe3281708b183b8d634492
thedevaman <devaman2508@gmail.com>
2026-02-21 09:35:08 +0530

    pass data with form

---
+++ fomdata.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import path from 'path'
+//import { fileURLToPath } from 'url'; //jab views folder me ejs by default views dhundhta h
+
+const app = express();
+
+//const __filename = fileURLToPath(import.meta.url);//jab views folder me ejs by default views dhundhta h
+//const __dirname = path.dirname(__filename);//jab views folder me ejs by default views dhundhta h
+
+app.use(express.urlencoded({extended:false}))//for body penchanane k liye
+
+app.set('view engine','ejs') 
+//app.set('views', path.join(__dirname, 'templates'));//jab views folder me ejs by default views dhundhta h isme "templates" new folder name
+
+app.get('/',(req,res)=>{
+
+    res.render("addUser")
+
+})
+
+app.post('/submit-user',(req,res)=>{
+
+    res.render("submitUsers",req.body)
+
+})
+
+
+app.listen(3200)
